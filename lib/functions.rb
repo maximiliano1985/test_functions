32c8040da3a2cf2d69fc3e2b02d1407b0a264a13
Maximiliano Giorgio <maximiliano_giorgio@yahoo.it>
2011-05-17 11:59:56 +0200

    Restarting the development of test functions library

---
+++ lib/functions.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+#  ga_my
+#
+#  Created by Carlos Maximiliano Giorgio Bort on 2011-03-06.
+#  Copyright (c) 2011 University of Trento. All rights reserved.
+#
+=begin
+As Hillstrom [1] proposed, here the starting points used in the optimizations are chosen from a box surrounding the standard starting point.
+One can evaluate the performances of the agorithm by counting the number of iterations required to converge to the solution.
+For each test function is defined a procedure, its name, its type and its starting points. The functions are defined in alphabetical order.
+=end
+
+module Test_fcn
+    # this method will test all the functions defined below
+    def test
+    end
+    private
+    
+    
+end
+
+
+
+# References:
+# [1] Hillstrom, K.E. A simulation test approach to the evaluation of nonlinear optimization algorithms. ACM Trans. Math. Softw. 3, 4 (1977), 305-315
---
+++ test.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#  ga_my
+#
+#  Created by Carlos Maximiliano Giorgio Bort on 2011-03-06.
+#  Copyright (c) 2011 University of Trento. All rights reserved.
+#
