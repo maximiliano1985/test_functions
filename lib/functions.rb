a4d9efa3f3b696ee6ae0d26ddd8bbf71fe1b584e
Maximiliano Giorgio <maximiliano_giorgio@yahoo.it>
2011-05-24 17:08:13 +0200

    YO BRO, it's working with 9 functions

--- lib/functions.rb
+++ lib/functions.rb
@@ -14,34 +14,69 @@ module Functions
   
   def load_fcns( n_fcns = "all" ) # list of all the test functions
     @functions = {
-      :rosenbrock => { :f          => @@rosenbrock,
-                       :class      => "nls",
-                       :start      => [-1.2, 1],
-                       :exax       => [1,1],
-                       :exac_f_abs => 0.0
+      :rosenbrock => { :f     => @@rosenbrock,
+                       :class => "sne-nls",
+                       :start => [-1.2, 1], # first trial point
+                       :x_abs => [1,1],     # absolute minimum abscissae
+                       :f_abs => 0.0        # absolute minimum ordinate
       },
       :freudenstein_and_roth => {
-                       :f          => @@freudenstein_and_roth,
-                       :class      => "nls",
-                       :start      => [0.5, -2],
-                       :exax_abs   => [5, 4],
-                       :exac_f_abs => 0.0,
-                       :exax_loc   => [11.41, -0.8968],
-                       :exac_f_loc => 48.9842
+                       :f     => @@freudenstein_and_roth,
+                       :class => "nls",
+                       :start => [0.5, -2],
+                       :x_abs => [5, 4],
+                       :f_abs => 0.0,
+                       :x_loc => [11.41, -0.8968],
+                       :f_loc => 48.9842
       },
       :powell_badly_scaled => {
-                       :f          => @@powell_badly_scaled,
-                       :class      => "nls",
-                       :start      => [0, 1],
-                       :exax_abs   => [1.098e-5, 9.106],
-                       :exac_f_abs => 0.0,
+                       :f     => @@powell_badly_scaled,
+                       :class => "sne-umi",
+                       :start => [0, 1],
+                       :x_abs => [1.098e-5, 9.106],
+                       :f_abs => 0.0
       },
       :brown_badly_scaled => {
-                       :f          => @@brown_badly_scaled,
-                       :class      => "nls",
-                       :start      => [1, 1],
-                       :exax_abs   => [1e6, 2e-6],
-                       :exac_f_abs => 0.0,
+                       :f     => @@brown_badly_scaled,
+                       :class => "umi",
+                       :start => [1, 1],
+                       :x_abs => [1e6, 2e-6],
+                       :f_abs => 0.0
+      },
+      :beale => {
+                       :f     => @@beale,
+                       :class => "umi",
+                       :start => [1,1],
+                       :x_abs => [3, 0.5],
+                       :f_abs => 0.0
+      },
+      :jennrich_and_sampson => {
+                       :f     => @@jennrich_and_sampson,
+                       :class => "nls",
+                       :start => [0.3,0.4],
+                       :x_abs => [0.2578, 0.2578],
+                       :f_abs => 124.362
+      },
+      :helical_valley => {
+                       :f     => @@helical_valley,
+                       :class => "sne-nls",
+                       :start => [-1,0,0],
+                       :x_abs => [ 1,0,0],
+                       :f_abs => 0.0
+      },
+      :bard => {
+                       :f     => @@bard,
+                       :class => "nls",
+                       :start => [1,1,1],
+                       :x_abs => [0.8406,-1e6, -1e6],
+                       :f_abs => 17.4286 ############## ambiguities here
+      },
+      :gaussian => {
+                       :f     => @@gaussian,
+                       :class => "umi",
+                       :start => [0.4, 1, 0],
+                       :x_abs => [0,0,0],############## ambiguities here
+                       :f_abs => 1.12793e-8
       }
     }
     i = 0
@@ -52,55 +87,75 @@ module Functions
   end
   
   
-  def residual(fcns={})
-    fcns.each_value{ |v| v[:residual] = (v[:f_now][i] - v[:exac_f_abs][i]) } # see the thopology of @functions hash
-    return fcns
-  end
+  def residual(fcns={}) ; fcns[:residual] = fcns[:f_cal] - fcns[:f_abs] ; return fcns ; end
   
-=begin
-  def increment(arg={})
-    inc = 0.0
-    arg[:x_now].each_index{ |i| inc += (arg[:x_now][i] - [:x_before][i]).abs }
-    return inc
-  end 
-  def rosenbrock_m(x)
-    raise "Dimension error" if x.size < 2
-    f_1 = 10*(x[1] - x[0]**2)
-    f_2 = 1 - x[0]
-    return f_1**2 + f_2**2  
-  end
-=end
+  private
+  
   @@rosenbrock = lambda do |x|
     raise "Dimension error" unless x.size == 2
-    f = []
-    f << 10*(x[1] - x[0]**2)
-    f << 1 - x[0]
-    return f.inject{ |v, n| v + n**2 }
+    (10*(x[1] - x[0]**2))**2 + (1 - x[0])**2
   end
   
   @@freudenstein_and_roth = lambda do |x|
     raise "Dimension error" unless x.size == 2
-    f = []
-    f << -13 + x[0] + ( (5 - x[1])*x[1] - 2 )*x[1]
-    f << -29 + x[0] + ( (x[1] + 1)*x[1] -14 )*x[1]
-    return f.inject{ |v, n| v + n**2 }
+    (-13 + x[0] + ( (5 - x[1])*x[1] - 2 )*x[1])**2 + ( -29 + x[0] + ( (x[1] + 1)*x[1] -14 )*x[1])**2
   end
   
   @@powell_badly_scaled = lambda do |x|
     raise "Dimension error" unless x.size == 2
-    f = []
-    f << 10**4*x[0]*x[1] - 1
-    f << Math::exp(-x[0]) + Math::exp(-x[1]) - 1.0001
-    return f.inject{ |v, n| v + n**2 }
+    (10**4*x[0]*x[1] - 1)**2 + (Math::exp(-x[0]) + Math::exp(-x[1]) - 1.0001)**2
   end
   
   @@brown_badly_scaled = lambda do |x|
     raise "Dimension error" unless x.size == 2
-    f = []
-    f << x[0] - 1e6
-    f << x[1] - 2e-6
-    f << x[0]*x[1] - 2
-    return f.inject{ |v, n| v + n**2 }
+    (x[0] - 1e6)**2 + (x[1] - 2e-6)**2 + (x[0]*x[1] - 2)**2
+  end
+  
+  @@beale = lambda do |x|
+    raise "Dimension error" unless x.size == 2
+    (1.5 - x[0]*(1-x[1]))**2 + (2.25 - x[0]*(1-x[1]**2))**2 + (2.625 - x[0]*(1-x[1]**3))**2
+  end
+  
+  @@jennrich_and_sampson = lambda do |x|
+    raise "Dimension error" unless x.size == 2
+    fcn = 0.0
+    10.times do |j|
+      i = j + 1
+      fcn += ( 2+2*i - ( Math::exp(i*x[0]) + Math::exp(i*x[1])  ) )**2
+    end
+    fcn
+  end
+  
+  @@helical_valley = lambda do |x|
+    raise "Dimension error" unless x.size == 3
+    theta = 1/Math::PI*Math::atan(x[1]/x[0])
+    theta += 1/2 if x[0] < 0
+    ( 10*(x[2]-10*theta) )**2 + ( 10*((x[0]**2 + x[1]**2)**0.5 - 1) )**2 + x[2]**2
+  end
+  
+  @@bard = lambda do |x|
+    raise "Dimension error" unless x.size == 3
+    y = [0.14, 0.18, 0.22, 0.25, 0.29, 0.32, 0.35, 0.39, 0.37, 0.58, 0.73, 0.96, 1.34, 2.10, 4.39]
+    fcn = 0.0
+    15.times do |j|
+      i = j + 1
+      v = 16 - i
+      w = [i, v].min
+      fcn += (y[j] - ( x[0] + i/(v*x[1] + w*x[2]) ))**2
+    end
+    fcn
+  end
+  
+  @@gaussian = lambda do |x|
+    raise "Dimension error" unless x.size == 3
+    y = [0.0009, 0.0044, 0.0175, 0.0540, 0.1295, 0.24020, 0.3521, 0.3989, 0.3521, 0.2420, 0.1295, 0.0540, 0.0175, 0.0044, 0.0009]
+    fcn = 0.0
+    15.times do |j|
+      i = j + 1
+      t = (8-i)/2
+      fcn += ( x[0]*Math::exp(-x[1]*(t-x[2])**2/2) - y[j] )**2
+    end
+    fcn
   end
 end # module Functions
 
--- lib/tester.rb
+++ lib/tester.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #  tester
 #
-#  Created by Carlos Maximiliano Giorgio Bort on 2011-03-06.
+#  Created by Carlos Maximiliano Giorgio Bort on 2011-04-24.
 #  Copyright (c) 2011 University of Trento. All rights reserved.
 #
 
@@ -13,66 +13,85 @@ require '../lib/functions'
 class Tester
   include Functions
   def initialize(args = {})
-    @cfg = {:n_fcns => "all" # number of test functions that will be evaluated
-      }
+    @cfg = {
+      :n_fcns => "all",      # number of test functions that will be evaluated
+      :seach_dom => [-10,10] #domain of values associated to the first trial solution
+    }
     @cfg.merge! args
     # based on the number of functions chosen to the test, load the function library
     @fcns = load_fcns(@cfg[:n_fcns]) # it is an array with the name of all functions that will be tested
     @file_name = "../results.txt"
-    File.open(@file_name, "w"){ |file| file.puts "Number of tested functions #{@cfg[:n_fcns]}
-Class of test functions: nls = nonlinear last squares, umi = unconstrained minimisation, sne = systems of nonlinear equations\n
-algorithm class n_iterations time exact_sol exact_f approx_sol approx_f residual" }
+    File.open(@file_name, "w"){ |file| file.puts "Number of tested functions #{@cfg[:n_fcns]}, starting domain: #{@cfg[:seach_dom]}
+Class of test functions:\nnls = nonlinear last squares\numi = unconstrained minimisation\nsne = systems of nonlinear equations\n
+n name class iterations time_[s] x_exact f_exact x_calculated f_calculated residual start_search" }
   end
   
-  def test(opt)
+  def test(extr_out)
     raise ArgumentError, "Block needed" unless block_given?
-    @fcns.count == 1 ? auxstr = "it is" : "they are"
-    puts "#{@fcns.count} functions will be tested, "+auxstr+":\n #{@fcns.keys.inspect}"
+    n = 1
+    @fcns.count == 1 ? auxstr = "it is" : auxstr = "they are"
+    puts "_"*60
+    puts "#{@fcns.count} functions will be tested, "+auxstr+":"
+    @fcns.each_key{ |k| puts k.to_s }
+    puts "_"*60
     
     ######### fare un metodo che imposti il primo tentativo
-    ary = {}
     @fcns.each do |k,f|
+      has = {} ; start_dom = {}
+      dim = f[:x_abs].count            # domain dimension of f
+      dim.times do |i|
+        start_dom[i.to_s.to_sym] = @cfg[:seach_dom]
+      end
+      opt = yield(start_dom)          # sets the optimisation algorithm
+      f[:name] = k.to_s
       start_t = Time.now
-      opt.loop(ary){|x| f[:f].call(x)} # runs the optimisation loop
+      opt.loop(has){|x| f[:f].call(x)} # runs the optimisation loop
       f[:time] = Time.now - start_t    # evaluates the time required to converge
-      res = yield(ary)                 # extracts the results
-      f[:x_cal] = res[0]                  # extracts the abscissae of the solution
+      res = extr_out.call(has)         # extracts the results
+      f[:x_cal] = res[0]               # extracts the abscissae of the solution
       f[:f_cal] = res[1]               # extracts the ordinate of the solution
       f[:n_it]  = res[2]               # extracts the number of iterations made
+      residual(f)
+      log( f, n )
+      n += 1
     end # fcns.each
     
-    residual(@fcns)
-    
-    
-    #puts self.method(fcns[0].to_sym).call([3,4])
-    #Functions::metaclass.superclass.method(:rosenbrock).call
-    #fcns.each{ |f| opt.loop{|p| f.to_sym.call(p)}}
-    #raise "ERROR: none argument passed to the test functions" if x == nil
-    #Functions::nonlinear_last_squares( , &block)
   end
   
-  def log(n = 1, hash={})
+  def log(hash={}, n = 1)
+    # the starting domain is considered far from the exact solution if...
+    hash[:x_abs].min < @cfg[:seach_dom].min || hash[:x_abs].max > @cfg[:seach_dom].max ? start_search = "far" : start_search = "near"
     File.open(@file_name, "a") do |file|
-      file.puts "#{n} #{hash[:f]} #{hash[:class]} #{hash[:n_it]}  #{hash[:time]}  #{hash[:x_abs]}  #{hash[:f_abs]}  #{hash[:x_cal]}  #{hash[:f_cal]}  #{hash[:residual]}"
+      file.print "#{n} #{hash[:name]} #{hash[:class]} #{hash[:n_it]} #{hash[:time]} #{hash[:x_abs]} #{hash[:f_abs]}   #{hash[:x_cal]} #{hash[:f_cal]}  #{hash[:residual]} #{start_search}\n"
     end
   end
 end 
 
 if __FILE__ == $0
   # Instantiate the optimizer, with tolerance and dimension
+=begin
   opt = GA::Optimizer.new( :tol => 1E-3,
     :p_mutation  => 0.2,
     :p_crossover => 0.8,
-    :i_o         => { :X =>[-5,10] , :Y=>[-10.23,5.234] },
-    :npop        => 50,
+    :npop        => 200,
     :ncr         => 150,
     :pconv       => false
   )
-  #opt.loop{|p| f.call(p)}
-  #p opt.iteration
-  tester = Tester.new(:n_fcns => 1) 
-  tester.test( opt ) do |a| # the block is used to extract the solution from the array of iterations
+=end
+  tester = Tester.new(:n_fcns => "all")
+  extract_out = lambda do |a| # procedure used to extract the solution from the array of iterations
     out_hash = a[(a.count-1).to_s.to_sym].first
     [out_hash[:chromosome], out_hash[:fitness], a.count]
+  end  
+  tester.test( extract_out ) do |domain| # block used to adapt the function domain dinamically
+    raise "Hash needed" unless domain.class == Hash
+    opt = GA::Optimizer.new( :tol => 1E-3,
+    :p_mutation  => 0.2,
+    :p_crossover => 0.8,
+    :npop        => 200,
+    :ncr         => 150,
+    :pconv       => false,
+    :i_o         => domain
+    )
   end
 end
